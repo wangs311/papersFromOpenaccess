8db166072d504779965ce061fd5aaa62340960e1
wangs311 <1216417625@qq.com>
2020-07-28 11:00:03 +0800

    URL update

--- getPapers.py
+++ getPapers.py
@@ -4,7 +4,7 @@
 @Author: wangshuo
 @Date: 2020-06-29 09:45:06
 @LastEditors: wangshuo
-@LastEditTime: 2020-06-29 15:54:44
+@LastEditTime: 2020-07-28 10:58:48
 '''
 
 from bs4 import BeautifulSoup
@@ -18,11 +18,26 @@ CONFERENCE = 'CVPR2020'
 # 关键字可以添加多个
 KEYWORDS = ['few-shot', 'few']
 # 是否下载
-DOWNLOAD_FLAG = False
+DOWNLOAD_FLAG = True
 
 # 跳转地址前缀
 PREFIX = 'https://openaccess.thecvf.com/'
-URL = PREFIX + CONFERENCE.upper()
+URL_BASE = PREFIX + CONFERENCE.upper()
+
+# CVPR2019 CVPR2020 URL变化调整
+DAY = []
+if CONFERENCE.upper() == 'CVPR2020':
+    DAY = ['2020-06-16', '2020-06-17', '2020-06-18']
+elif CONFERENCE.upper() == 'CVPR2019':
+    DAY = ['2019-06-18', '2019-06-19', '2019-06-20']
+
+if len(DAY) > 0:
+    URL = []
+    for day in DAY:
+        URL.append(URL_BASE + '?day=' + day)
+else:
+     URL = [URL_BASE]
+
 # 保存路径
 SAVEPATH = 'papers_' + KEYWORDS[0] + '_' + CONFERENCE
 # 保存文件前缀
@@ -38,18 +53,10 @@ DICT_REP = {
 
 
 def main():
-    # 请求HTML
-    html = urlopen(URL).read().decode('utf-8')
-    soup = BeautifulSoup(html, features='lxml')
-    
-    # 找到所有class为ptitle的标签 
-    paper_titles = soup.find_all('dt', {"class": "ptitle"})
-    paper_all_nums = len(paper_titles)
-
     # 创建文件夹
     if not os.path.exists(SAVEPATH):
         os.mkdir(SAVEPATH)
-
+    
     # 用于存放文章标题
     titles_list = []
     # 用于存放文章链接
@@ -57,31 +64,40 @@ def main():
     # 计数器
     papers_num = 0
 
-    for i in range(paper_all_nums):
-        paper_title = paper_titles[i].get_text()
-        # 遍历，是否包含关键字
-        for kw in KEYWORDS:
-            if paper_title.lower().find(kw) != -1:
-                papers_num += 1
-                titles_list.append(paper_title)
-                # 文件名
-                file_name = FILEPREFIX +  rep(paper_title) + '.pdf'
-                # 保存路径
-                file_path = os.path.join(SAVEPATH, file_name)
-                # 文章链接
-                pdf_url = PREFIX + paper_titles[i].next_sibling.next_sibling.next_sibling.next_sibling.a['href']
-                links_list.append(pdf_url)
-                
-                # 下载文章
-                if DOWNLOAD_FLAG:
-                    r = requests.get(pdf_url)
-                    print(papers_num, paper_title)
-                    with open(file_path , 'wb') as f:
-                        f.write(r.content)
-                # 避免重复
-                break
+    # 请求HTML
+    for url in URL:
+        html = urlopen(url).read().decode('utf-8')
+        soup = BeautifulSoup(html, features='lxml')
+    
+        # 找到所有class为ptitle的标签 
+        paper_titles = soup.find_all('dt', {"class": "ptitle"})
+        paper_all_nums = len(paper_titles)
 
-    save_txt(titles_list, links_list, nums=papers_num)
+        for i in range(paper_all_nums):
+            paper_title = paper_titles[i].get_text()
+            # 遍历，是否包含关键字
+            for kw in KEYWORDS:
+                if paper_title.lower().find(kw) != -1:
+                    papers_num += 1
+                    titles_list.append(paper_title)
+                    # 文件名
+                    file_name = FILEPREFIX +  rep(paper_title) + '.pdf'
+                    # 保存路径
+                    file_path = os.path.join(SAVEPATH, file_name)
+                    # 文章链接
+                    pdf_url = PREFIX + paper_titles[i].next_sibling.next_sibling.next_sibling.next_sibling.a['href']
+                    links_list.append(pdf_url)
+                    
+                    # 下载文章
+                    if DOWNLOAD_FLAG:
+                        r = requests.get(pdf_url)
+                        print(papers_num, paper_title)
+                        with open(file_path , 'wb') as f:
+                            f.write(r.content)
+                    # 避免重复
+                    break
+
+    save_markdown(titles_list, links_list, nums=papers_num)
 
 
 def rep(rawstr, dict_rep=DICT_REP):
@@ -94,7 +110,7 @@ def rep(rawstr, dict_rep=DICT_REP):
     return rawstr
 
 
-def save_txt(titles, links, nums):
+def save_markdown(titles, links, nums):
     '''
     @Author: wangshuo
     @description: generate a paper list
